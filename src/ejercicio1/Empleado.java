7dfd520681e46d9564ea476f99ad01743483f95d
Luxary <f.leiton001@gmail.com>
2025-08-23 13:38:34 -0300

    Create Empleado.java

---
+++ src/ejercicio1/Empleado.java
@@ -0,0 +1,32 @@
+package ejercicio1;
+
+public class Empleado extends Persona{
+	private final int legajo;
+	private String puesto;
+	
+	//Getters y setters
+	public String getPuesto() {
+		return puesto;
+	}
+
+	public void setPuesto(String puesto) {
+		this.puesto = puesto;
+	}
+
+	public int getLegajo() {
+		return legajo;
+	}
+
+	//Constructors
+	public Empleado () {
+		super();
+		this.legajo = 0; //Provisorio para permitir compilacion, cambiar por el metodo que genere el legajo automaticamente
+	}
+	
+	public Empleado (String puesto) {
+		this.puesto = puesto;
+		this.legajo = 0; //Provisorio para permitir compilacion, cambiar por el metodo que genere el legajo automaticamente
+	}
+	
+	
+}
